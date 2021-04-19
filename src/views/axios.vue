e1db04822f9e2c15e15cd69c81b4bebf43c9f500
董-longjiang <767624298@qq.com>
2021-04-19 17:11:23 +0800

    init

---
+++ src/views/axios.vue
@@ -0,0 +1,16 @@
+<template>
+111
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'axios',
+  components: {
+  }
+})
+</script>
+
+<style>
+</style>
---
+++ src/views/vuex.vue
@@ -0,0 +1,16 @@
+<template>
+333
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'vuex',
+  components: {
+  }
+})
+</script>
+
+<style>
+</style>
